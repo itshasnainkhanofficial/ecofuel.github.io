a876886e0e168c1ab959ddfb86dc9fd2665f54a8
hasnainkhan <hasnainkhankqs@gmail.com>
2019-08-29 20:48:49 +0500

    navigation bar and header completed

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+
+$(function(){
+   var scrollLink = $('.scrolltab');
+  scrollLink.click(function(e) {
+    e.preventDefault();
+
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
+
+
+  $(window).scroll(function(){
+
+    
+      var sec1 = $("#sec_1").offset().top;
+      var win = $(window).scrollTop();
+
+      if(win >= sec1 ){
+        $(".navbar").addClass("animateBar");
+      }
+      else{
+        $(".navbar").removeClass("animateBar");
+      }
+      
+      
+    var scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function() {
+      
+      var sectionOffset = $(this.hash).offset().top - 20;
+      
+      if ( sectionOffset <= scrollbarLocation ) {
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+        
+      }
+    })
+  })
+
+  
+});
